1fc0dab5f8a83bee5e5cd9f21f3125d8b9485729
CHANDRU M <mc9650309@gmail.com>
2026-05-16 11:30:25 +0530

    Create database and employees table in db.sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE company;
+
+USE company;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    department VARCHAR(100)
+);
